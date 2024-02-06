6d2c922b87f9a095b256b100c09fd38d2893ddf0
lindi <lindi.harding@gmail.com>
2024-02-06 19:14:44 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TodoItem from "./components/TodoItem";
+
+function App(){
+  return <>
+
+  <h1>My todo list</h1>
+  <TodoItem/>
+  
+  </>;
+}
+
+export default App;
---
+++ src/components/TodoItem/index.jsx
@@ -0,0 +1,4 @@
+function TodoItem(){
+    return<div>Buy Ben a beer!</div>;
+}
+export default TodoItem;
