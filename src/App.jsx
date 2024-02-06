1453f027720792126060c0847cd59fed0adbe21e
lindi <lindi.harding@gmail.com>
2024-02-06 19:41:09 +0800

    using react state and using map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import {useState} from 'react';
 import TodoItem from "./components/TodoItem";
 
 function App(){
+  const [todo, setTodo] = useState([
+    "Buy Jeremy nothing.",
+    "Buy Ben Beer.",
+    "Buy Ben Food.",
+    "Fine, get jeremy a beer"
+
+  ]);
+  function generateTodoComponent(todo,index){
+    return <TodoItem key ={index} text={todo}/>;
+  }
+
   return <>
-
   <h1>My todo list</h1>
-  <TodoItem text ="Buy Jeremy nothing."/>
-  <TodoItem text ="Buy Ben Beer."/>
-  <TodoItem text ="Buy Ben Food."/>
-
+  {todo.map(generateTodoComponent)}
   </>;
 }
 
