eecfc3d5b20544917684e563efa3658418d16f77
lindi <lindi.harding@gmail.com>
2024-02-06 19:45:25 +0800

    renaming todos and todoitems for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import {useState} from 'react';
 import TodoItem from "./components/TodoItem";
 
 function App(){
-  const [todo, setTodo] = useState([
+  const [todoItems, setTodoItems] = useState([
     "Buy Jeremy nothing.",
     "Buy Ben Beer.",
     "Buy Ben Food.",
     "Fine, get jeremy a beer"
 
   ]);
-  function generateTodoComponent(todo,index){
-    return <TodoItem key ={index} text={todo}/>;
+  function generateTodoComponent(item,index){
+    return <TodoItem key ={index} text={item}/>;
   }
 
   return <>
   <h1>My todo list</h1>
-  {todo.map(generateTodoComponent)}
+  {todoItems.map(generateTodoComponent)}
   </>;
 }
 
